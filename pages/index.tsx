8922dc67a453be41abc3530dc6c18fedfa674787
samirgannem <sgannem@gmail.com>
2021-04-04 12:58:28 -0400

    registro email

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,103 +1,62 @@
-// import Head from 'next/head'
-
-import { Heading, Grid, Flex, Link, Button, Text } from '@chakra-ui/core'
-import Divider from '../components/Divider'
+import { FormEvent, useState } from 'react'
+import { Flex, Image, Button, Text } from '@chakra-ui/core'
 import Input from '../components/Input'
+import axios from 'axios';
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  
+  function handleSignUpToNewsletter(event: FormEvent) {
+    event.preventDefault();
+    axios.post('/api/subscribe', { email });
+  }
+
   return (
-    <Grid
+    <Flex
       as="main"
       height="100vh"
-      templateColumns="1fr 480px 480px 1fr"
-      templateRows="1fr 480px 1fr"
-      templateAreas="
-        '. . . .'
-        '. logo form .'
-        '. . . .'
-      "
       justifyContent="center"
       alignItems="center"
     >
-      <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
-        <img src="/rocketseat.svg" alt="Rocketseat" />
-
-        <Heading size="2xl" lineHeight="shorter" marginTop={16}>
-          Faça seu login na plataforma
-        </Heading>
-      </Flex>
-
-      <Flex 
-        gridArea="form"
-        height="100%"
-        backgroundColor="gray.700"
+      <Flex
+        as="form"
+        onSubmit={handleSignUpToNewsletter}
+        backgroundColor="gray.900"
         borderRadius="md"
         flexDir="column"
         alignItems="stretch"
-        padding={16}
+        padding={8}
+        marginTop={4}
+        width="100%"
+        maxW="400px"
       >
-        <Input
-          placeholder="E-mail"
-        />
+        <Image marginBottom={8} src="/informaticaSG.png" alt="Informática SG" />
+
+        <Text textAlign="center" fontSize="sm" color="gray.300" marginBottom={2}>
+          Assine a Informática SG e receba os melhores serviços de TI!
+        </Text>
 
         <Input
-          placeholder="Senha"
+          placeholder="Seu melhor e-mail"
           marginTop={2}
+          value={email}
+          background="white"
+          color="black"
+          onChange={e => setEmail(e.target.value)}
         />
-
-        <Link
-          alignSelf="flex-start"
-          marginTop={2}
-          fontSize="sm"
-          color="purple.600"
-          fontWeight="bold"
-          _hover={{ color: 'purple.500' }}
-        >
-          Esqueci minha senha
-        </Link>
 
         <Button
-          backgroundColor="purple.500"
+          type="submit"
+          backgroundColor="white"
           height="50px"
           borderRadius="sm"
           marginTop={6}
-          _hover={{ backgroundColor: 'purple.600' }}
+          color='black'
+          _hover={{ backgroundColor: 'gray.500' }}
         >
-          ENTRAR
+          INSCREVER
         </Button>
-
-        <Text
-          textAlign="center"
-          fontSize="sm"
-          color="gray.300"
-          marginTop={6}
-        >
-          Não tem uma conta? {" "}
-          <Link
-            color="purple.600"
-            fontWeight="bold"
-            _hover={{ color: 'purple.500' }}
-          >
-            Registre-se
-          </Link>
-        </Text>
-
-        <Divider />
-
-        <Flex alignItems="center">
-          <Text fontSize="sm">Ou entre com</Text>
-          <Button
-            height="50px"
-            flex="1"
-            backgroundColor="gray.600"
-            marginLeft={6}
-            borderRadius="sm"
-            _hover={{ backgroundColor: 'purple.500' }}
-          >
-            GITHUB
-          </Button>
-        </Flex>
       </Flex>
-    </Grid>
+    </Flex>
   )
 }
